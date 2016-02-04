084e9dbc5beacc21ba4485451659b900aa43b3ec
Faraaz Khan <faraaz@rationalizeit.us>
2016-02-03 23:19:32 -0600

    still wip

--- scripts/pair_me.js
+++ scripts/pair_me.js
@@ -1,11 +1,9 @@
 module.exports = function(robot) {
-  var ec2Helper = require('../lib/ec2helper');
+  var pairingBox = require('../lib/pairingbox');
   var nameGenerator = require('../lib/namegenerator');
   robot.respond(/pair me/i, function(res) {
-    msg = ec2Helper.createInstance().then(
-      function(req) { res.send(req) } ,
-      function(err) { res.send(err) }
-    );
+    msg = pairingBox.create();
+    res.send(msg);
   });
 };
 
